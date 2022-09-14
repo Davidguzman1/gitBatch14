78b14845baae8c519ab29e2e68120a2530b3ec97
Davidguzman1 <Daviddaguz75@gmail.com>
2022-09-14 16:38:15 -0400

    Second commit

--- src/com/learningGit/syntax/Test3.java
+++ src/com/learningGit/syntax/Test3.java
@@ -1,11 +1,9 @@
 package com.learningGit.syntax;
 
-public class Teset1 {
+public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Learning git");
-		System.out.println("Making a change");
 
 	}
 
