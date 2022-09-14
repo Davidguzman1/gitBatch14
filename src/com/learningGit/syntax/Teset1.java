afac95fe09b5927b704fabec0ffa30f959195891
Davidguzman1 <Daviddaguz75@gmail.com>
2022-09-14 16:31:42 -0400

    first commit

---
+++ src/com/learningGit/syntax/Teset1.java
@@ -0,0 +1,11 @@
+package com.learningGit.syntax;
+
+public class Teset1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Learning git");
+
+	}
+
+}
